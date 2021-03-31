ed03f2a07dc75527085b7fcf3b08ef51069307b8
vishalicious213 <vishalicious213@gmail.com>
2021-03-31 12:43:28 -0700

    added initial placeholder pages

--- pages/donate.js
+++ pages/donate.js
@@ -1,17 +1,17 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export default function About() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Breast Cancer Comfort Foundation</title>
+        <title>Donate | Breast Cancer Comfort Foundation</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          HOME
+          DONATE
         </h1>
       </main>
 
