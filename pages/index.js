6395a97cc0fc0cef3fd7cb270679708c7b53e112
vishalicious213 <vishalicious213@gmail.com>
2021-04-04 15:10:08 -0700

    added & styled 3 sections to index

--- pages/index.js
+++ pages/index.js
@@ -17,12 +17,34 @@ export default function Home() {
         <article className='pink-container'>
           <p className='no-space-below'><b>Breast Cancer Comfort Foundation</b></p>
           <p className='no-space-above'><b>January 17, 2021</b></p>
-
           <p>Nearly five years ago, Breast Cancer Comfort was founded by survivor Debi Cavolo. Like many other women, Debi felt there was an absence of community and conversation about breast cancer. Women diagnosed with breast cancer face issues that significantly impact their quality and length of life. With this in mind, BCC has since set out to address the unique needs of women through the distribution of comfort baskets. Assembled with empathy, a dash of hope, and a few sprinkles of inspiration, these baskets strive to promote the spiritual, emotional, and mental healing of breast cancer patients. BCC has partnered with numerous hospitals and rehab centers on Long Island for the delivery of baskets to patients, at no cost to the recipient . We highly encourage requests for baskets. After all, BCC strives to offer women the sense of comradeship and understanding.</p>
         </article>
+
+        <section className='pink-container'>
+          <h2 className='h2'>Our Mission</h2>
+          <p className='no-space-above'>The Breast Cancer Comfort Foundation aims to offer encouragement, support and laughter to individuals whose lives have been forever changed by curative or preventative breast cancer treatment. We do this by delivering unique “Comfort Baskets” and “Chemo Comfort Bags” containing thoughtfully chosen gifts and personal care items.</p>
+        </section>
+
+        <section className='grey-container'>
+          <h2 className='h2'>Our Project</h2>
+          <p className='no-space-above'>Our current project, “Comfort Baskets”, was created to aid those who have recently undergone breast cancer surgery, at no cost to the recipient.  We have included treatments such as chemotherapy and created “Chemo Comfort Bags”. These “Comfort Baskets and Bags“ are central to our cause, as they give individuals reassurance in knowing they are not alone. We want to inspire and help them to realize the power of their inner strength.</p>
+        </section>
+
+        <section className='pink-container'>
+          <h2 className='h2'>Pay It Forward</h2>
+          <p className='no-space-above'>If you would like to contribute to this or any of our many other future projects, please consider making a donation. Any amount is greatly appreciated and it all goes directly to funding “Comfort Baskets” and “Chemo Comfort Bags”.</p>
+        </section>
       </main>
 
       <Footer />
+
+      <style jsx>
+        {`
+          .h2 {
+            margin-bottom: .5rem;
+          }
+        `}
+      </style>
     </div>
   )
 }
