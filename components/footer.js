2f614b9a82ef0a77fdcca6d062bc9bbf009b45e7
vishalicious213 <vishalicious213@gmail.com>
2021-04-05 18:05:33 -0700

    styling footer

--- components/footer.js
+++ components/footer.js
@@ -4,18 +4,18 @@ export default function Footer() {
     return (
         <>
         <footer className='footer'>
-            <div>
+            <div className='footer-text'>
                 Breast Cancer Comfort Foundation is a 501(C)(3) non-profit organization recognized by the Internal Revenue Service, and all donations to the Breast Cancer Comfort Foundation are tax-deductible in accordance with IRS regulations.
             </div>
 
-            <div>
-                <p>36C West Green Village, Hicksville, NY 11801</p>
-                <p>(516) 490-4255   |   info@breastcancercomfort.org</p>
-                <p>Ⓒ 2021 Breast Cancer Comfort Foundation</p>
-                <p>All rights reserved</p>
+            <div className='footer-text'>
+                <p className='no-space-below'>36C West Green Village, Hicksville, NY 11801</p>
+                <p className='no-space-above min-space-below'>(516) 490-4255   |   info@breastcancercomfort.org</p>
+                <p className='no-space-below'>Ⓒ 2021 Breast Cancer Comfort Foundation</p>
+                <p className='no-space-above'>All rights reserved</p>
             </div>
 
-            <div>
+            <div className='footer-social'>
                 <a href="https://twitter.com/BreastComfort">
                     <img src='/twitter.jpg' alt='Twitter' />
                 </a>
@@ -31,15 +31,40 @@ export default function Footer() {
         <style jsx>
             {`
             .footer {
+                // outline: 1px solid red;
+                color: gainsboro;
+                background: black;
+                font-size: .75rem;
                 width: 100%;
-                height: 100px;
                 display: flex;
                 justify-content: center;
                 align-items: center;
+                padding-bottom: 1rem;
+              }
+
+              .footer-text {
+                //   outline: 1px solid orange;
+                  width: 33%;
+                  display: flex;
+                  flex-direction: column;
+                  align-items: center;
+                  text-align: center;
+              }
+
+              .min-space-below {
+                  margin-bottom: 0;
+              }
+
+              .footer-social {
+                //   outline: 1px solid orange;
+                  width: 33%;
+                  display: flex;
+                  justify-content: center;
               }
 
               .footer img {
-                margin-left: 0.5rem;
+                width: 2.5rem;
+                margin: 0 .5rem;
               }
 
               .footer a {
