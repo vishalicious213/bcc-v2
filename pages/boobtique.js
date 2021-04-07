dded930646f60dff0ba098d20c8e55453a65007d
vishalicious213 <vishalicious213@gmail.com>
2021-04-06 19:16:29 -0700

    adding text to boobtique

--- pages/boobtique.js
+++ pages/boobtique.js
@@ -17,6 +17,17 @@ export default function Boobtique() {
           <div className='img-container'>
             <img src='/boobtique/boobtique.jpg' alt='' />
           </div>
+
+          <section className='pink-container gift-container'>
+            <div className='gift-img-container'>
+                <img className='gift-img' src='gift.png' alt='wrapped gift' />
+            </div>
+            <div className='gift-text-container'>
+                <h2 className='h2'>Welcome to our Boobtique!</h2>
+                <p className='no-space-above'>We are getting ready to open our doors to the public. If you know someone who is undergoing treatment for breast cancer and you don’t know what to give, then let us help you.</p>
+                <p>Handmade hearts, comfort bags, and custom gift baskets will be available soon. Select any one of our specialty items and we will ship to you, or anyone you choose, within 48 hours.</p>
+            </div>
+          </section>
       </main>
 
       <Footer />
