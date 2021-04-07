bf0b1f18bc2a4342c9330535fc5d8960626d2006
vishalicious213 <vishalicious213@gmail.com>
2021-04-07 08:02:24 -0700

    added supporters.js placeholder page

---
+++ pages/supporters.js
@@ -0,0 +1,22 @@
+import Head from 'next/head'
+import Nav from '../components/nav'
+import Footer from '../components/footer'
+import styles from '../styles/Home.module.css'
+
+export default function Support() {
+  return (
+    <div className={styles.container}>
+        <Head>
+            <title>Supporters | Breast Cancer Comfort Foundation</title>
+            <link rel="icon" href="/favicon.ico" />
+        </Head>
+
+        <Nav />
+
+        <main className={styles.main}>
+            <h1 className={styles.title}>SUPPORTERS</h1>
+        </main>
+        <Footer />
+    </div>
+  )
+}
