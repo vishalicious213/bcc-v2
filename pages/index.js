1dbb2426d124ca46485044d72b0f461ae2890966
vishalicious213 <vishalicious213@gmail.com>
2021-04-07 21:41:33 -0700

    added ribbon to index/message area

--- pages/index.js
+++ pages/index.js
@@ -15,10 +15,15 @@ export default function Home() {
       <Nav />
 
       <main className={styles.main}>
-        <article className='pink-container'>
-          <p className='no-space-below'><b>Breast Cancer Comfort Foundation</b></p>
-          <p className='no-space-above'><b>January 17, 2021</b></p>
-          <p>Nearly five years ago, Breast Cancer Comfort was founded by survivor Debi Cavolo. Like many other women, Debi felt there was an absence of community and conversation about breast cancer. Women diagnosed with breast cancer face issues that significantly impact their quality and length of life. With this in mind, BCC has since set out to address the unique needs of women through the distribution of comfort baskets. Assembled with empathy, a dash of hope, and a few sprinkles of inspiration, these baskets strive to promote the spiritual, emotional, and mental healing of breast cancer patients. BCC has partnered with numerous hospitals and rehab centers on Long Island for the delivery of baskets to patients, at no cost to the recipient . We highly encourage requests for baskets. After all, BCC strives to offer women the sense of comradeship and understanding.</p>
+        <article className='pink-container message'>
+          <div id='ribbon'>
+            <img src='/breast-cancer-ribbon.png' />
+          </div>
+          <div id='message-text'>
+            <p className='no-space-below'><b>Breast Cancer Comfort Foundation</b></p>
+            <p className='no-space-above'><b>January 17, 2021</b></p>
+            <p>Nearly five years ago, Breast Cancer Comfort was founded by survivor Debi Cavolo. Like many other women, Debi felt there was an absence of community and conversation about breast cancer. Women diagnosed with breast cancer face issues that significantly impact their quality and length of life. With this in mind, BCC has since set out to address the unique needs of women through the distribution of comfort baskets. Assembled with empathy, a dash of hope, and a few sprinkles of inspiration, these baskets strive to promote the spiritual, emotional, and mental healing of breast cancer patients. BCC has partnered with numerous hospitals and rehab centers on Long Island for the delivery of baskets to patients, at no cost to the recipient . We highly encourage requests for baskets. After all, BCC strives to offer women the sense of comradeship and understanding.</p>
+          </div>
         </article>
 
         <div className='button-container'>
@@ -76,6 +81,46 @@ export default function Home() {
       </main>
 
       <Footer />
+
+      <style jsx>
+        {`
+        #ribbon {
+          display: none;
+        }
+
+        @media only screen and (min-width: 768px) {
+          .message {
+            display: flex;
+            align-items: flex-start;
+            justify-content: space-between;
+          }
+  
+          #message-text {
+            width: 75%;
+          }
+  
+          #ribbon {
+            display: block;
+            padding-top: 1rem;
+            width: 20%;
+          }
+  
+          #ribbon img {
+            width: 100%;
+          }
+        }
+
+        @media only screen and (min-width: 1024px) {
+          .message {
+            align-items: center;
+          }
+
+          #ribbon {
+            padding-top: 0;
+          }
+        }
+        `}
+      </style>
     </div>
   )
 }
