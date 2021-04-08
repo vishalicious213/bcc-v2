1bc5f3a4769543ff75de93bddc84f9f259d0d106
vishalicious213 <vishalicious213@gmail.com>
2021-04-07 18:30:50 -0700

    hid navbar < 768 and adding burger menu

--- components/nav.js
+++ components/nav.js
@@ -11,6 +11,7 @@ export default function Nav() {
                 <img src='/logo.png' alt='logo' />
             </div>
 
+            {/* Navbar for 768+ */}
             <nav id='menu'>
                 <Link href='/'><a>Home</a></Link>
                 <Link href='/about'><a>About Us</a></Link>
@@ -28,8 +29,17 @@ export default function Nav() {
                         <Link href='/share'><a className='a center-link'>Share Your Story</a></Link>
                     </div>
                 </div>
-                
                 <Link href='/news'><a>News</a></Link>
+            </nav>
+
+            {/* Hamburger menu for < 768 */}
+            <nav id='burger'>
+                <a href='/'>Home</a>
+                <a href='/about'>About Us</a>
+                <a href='/boobtique'>Boobtique</a>
+                <a href='/donate'>Donate</a>
+                <a href='/volunteer'>Volunteer</a>
+                <a href='/news'>News</a>
             </nav>
 
             <style jsx>
@@ -49,51 +59,66 @@ export default function Nav() {
                     width: 100%;
                 }
 
-                #menu {
+                #burger {
                     display: flex;
+                    flex-direction: column;
                     align-items: center;
                     color: gainsboro;
                 }
 
-                a {
-                    font-size: .9rem;
-                    width: 6rem;
-                    padding: .5rem;
-                    text-align: center;
-                    border: 1px solid #292c2f;
-                }
-
-                a:hover {
-                    color: black;
-                    background: gainsboro;
-                }
-
-                .hover-menu {
-                    width: 6rem;
-                    position: relative;
-                    display: inline-block;
-                    text-align: center;
-                }
-
-                .a {
-                    display: block;
-                    width: 100%;
-                }
-
-                .dropdown {
+                #menu {
                     display: none;
-                    position: absolute;
-                    width: 8rem;
-                    z-index: 1;
                 }
 
-                .center-link {
-                    transform: translateX(-.75rem);
-                    background: #292c2f;
-                }
+                /* 768 PX */
 
-                .hover-menu:hover .dropdown {
-                    display: block;
+                @media only screen and (min-width: 768px) {
+                    #menu {
+                        display: flex;
+                        align-items: center;
+                        color: gainsboro;
+                    }
+    
+                    a {
+                        font-size: .9rem;
+                        width: 6rem;
+                        padding: .5rem;
+                        text-align: center;
+                        border: 1px solid #292c2f;
+                    }
+    
+                    a:hover {
+                        color: black;
+                        background: gainsboro;
+                    }
+    
+                    .hover-menu {
+                        width: 6rem;
+                        position: relative;
+                        display: inline-block;
+                        text-align: center;
+                    }
+    
+                    .a {
+                        display: block;
+                        width: 100%;
+                    }
+    
+                    .dropdown {
+                        display: none;
+                        position: absolute;
+                        width: 8rem;
+                        z-index: 1;
+                    }
+    
+                    .center-link {
+                        transform: translateX(-.75rem);
+                        background: #292c2f;
+                    }
+    
+                    .hover-menu:hover .dropdown {
+                        display: block;
+                    }
                 }
             `}
             </style>
