f278b9b168c72a382299dd22fe2ef685e730e159
vishalicious213 <vishalicious213@gmail.com>
2021-04-10 13:29:02 -0700

    News: adding list of news/blog posts

--- pages/news.js
+++ pages/news.js
@@ -3,6 +3,11 @@ import Nav from '../components/nav'
 import Footer from '../components/footer'
 import styles from '../styles/Home.module.css'
 
+const posts = [
+  {id: '001', date: 'Jan 17, 2021', url: '', title: 'How We Started and What We Do', author: 'Alaha Nasari', excerpt: 'Nearly five years ago, Breast Cancer Comfort was founded by survivor Deborah Cavolo. Like many other women, Debi felt there was an absence of community and conversation about breast cancer. Women diagnosed with breast cancer face issues that significantly impact their...'},
+  // {date: '', url: '', title: '', author: '', excerpt: ''},
+]
+
 export default function News() {
   return (
     <div className={styles.container}>
@@ -17,8 +22,25 @@ export default function News() {
 
         <main className={styles.main}>
             <h1 className={styles.title}>NEWS</h1>
+
+            {posts.map(post =>
+              <div key={post.id} className='post'>
+                <h2>{post.title}</h2>
+                <p>by {post.author} | {post.date}</p>
+                <p>{post.excerpt}</p>
+
+              </div>
+            )}
         </main>
         <Footer />
+
+        <style jsx>
+          {`
+          .post {
+            color: white;
+          }
+          `}
+        </style>
     </div>
   )
 }
