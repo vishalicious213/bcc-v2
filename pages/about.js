6b0011c23ca8e2f43a413cc8808e74e666603038
vishalicious213 <vishalicious213@gmail.com>
2021-11-29 16:02:52 -0800

    changed video on about.js to 'featured on news 12' video

--- pages/about.js
+++ pages/about.js
@@ -31,7 +31,7 @@ export default function About() {
           <div className='media-container'>
             <div className='video'>
                 <div className='videoContainer'>
-                    <iframe width="560" height="315" src="https://www.youtube.com/embed/O0cpKBZFAck" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/MHCwlVk1UxQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 </div>
             </div>
           </div>
