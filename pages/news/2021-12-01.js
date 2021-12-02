64a783e79fa8ed026cb33ce03720b57ec7b76ab2
vishalicious213 <vishalicious213@gmail.com>
2021-12-01 17:48:48 -0800

    added images to /news/2021-12-01 & formatted/styled lists

--- pages/news/2021-12-01.js
+++ pages/news/2021-12-01.js
@@ -20,25 +20,31 @@ export default function Post() {
                 <h2 className='no-space-below'>BCC Collaboration with Memorial Sloan Kettering</h2>
                 <p className='no-space-above'>by <span>Maeryam Nasari & Zahel Nasari</span> | Dec 1, 2021</p>
                 <p>The Breast Cancer Comfort Foundation is proud to announce its most recent collaboration with Memoral Sloan Kettering, NYC’s largest cancer treatment center. CUNY School of Medicine students who are members of the American Medical Women’s Association delivered around 20 bags and 5 baskets to breast cancer patients who are currently undergoing chemotherapy.</p>
-                <p>Pictured:</p>
-                <p>Meaghan Lane, Assistant Manager at Memorial Sloan Kettering</p>
-                <p>CUNY School of Medicine Students:</p>
-                <p>Rachelle Monteau</p>
-                <p>Udeme Udom</p>
-                <p>Layla Morgan</p>
-                <p>Amina Nasari</p>
-                <p>Mariam Qureishi</p>
-
-
-
-
-                <p>#cans4cancerNY #togetherwemakeadifference #recycling4acure</p>
-                <p><a className='link' target="_blank" rel="noopener" href='https://recycling4acure.org/'>Pink Cans 4 Cancer</a> is a Garden City NY non-profit organization, and movement of hope & sustainability to help people with cancer while saving the planet!</p>
-                <p>Check out our FaceBook page <a className='link' target="_blank" rel="noopener" href='https://www.facebook.com/breastcancercomfort/'>Breast Cancer Comfort</a> and the FaceBook page for <a className='link' target="_blank" rel="noopener" href='https://www.facebook.com/Cans4CancerNY'>Pink Cans 4 Cancer</a> to find out how you can #makeadifference</p>
 
                 <div className='img-container'>
-                    <img src='/news/2021-11-12.jpg' alt='' />
+                    <img src='/news/mskcc-4.jpg' alt='' />
                 </div>
+
+                <p>Pictured:</p>
+                <ul>
+                  <li>Meaghan Lane, Assistant Manager at Memorial Sloan Kettering</li>
+                </ul>
+
+                <p>CUNY School of Medicine Students:</p>
+                <ul>
+                  <li>Rachelle Monteau</li>
+                  <li>Udeme Udom</li>
+                  <li>Layla Morgan</li>
+                  <li>Amina Nasari</li>
+                  <li>Mariam Qureishi</li>
+                </ul>
+
+                <div className='img-gallery'>
+                    <img className='gallery-img' src='/news/mskcc-1.jpg' alt='' />
+                    <img className='gallery-img' src='/news/mskcc-2.jpg' alt='' />
+                    <img className='gallery-img' src='/news/mskcc-3.jpg' alt='' />
+                </div>
+
             </div>
         </main>
 
@@ -46,6 +52,10 @@ export default function Post() {
 
         <style jsx>
           {`
+          li {
+            color: gainsboro;
+          }
+
           .post {
             width: 80%;
             margin: 3rem auto;
@@ -77,6 +87,17 @@ export default function Post() {
 
           img {
               width: 100%;
+          }
+
+          .img-gallery {
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: space-between;
+            margin-top: 2rem;
+          }
+
+          .gallery-img {
+            width: 30%;
           }
           `}
         </style>
