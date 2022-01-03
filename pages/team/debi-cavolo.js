4ebba610c2dff5df1fc201b0a30ad492735bab61
vishalicious213 <vishalicious213@gmail.com>
2022-01-03 11:33:01 -0800

    team profiles: gave social media icons rounded border radius

--- pages/team/debi-cavolo.js
+++ pages/team/debi-cavolo.js
@@ -122,6 +122,7 @@ export default function Teammate() {
           .icon {
               width: 2.5rem;
               height: 2.5rem;
+              border-radius: 50%;
           }
 
           .fb {
