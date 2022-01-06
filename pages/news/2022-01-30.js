87c7341610fe1b594a2a3e30b1fb0aad41a37512
vishalicious213 <vishalicious213@gmail.com>
2022-01-05 16:35:01 -0800

    news/2022-01-30: added FB feed

--- pages/news/2022-01-30.js
+++ pages/news/2022-01-30.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import styles from '../../styles/Home.module.css'
+import FB from '../../components/fb'
 
 export default function Post() {
   return (
@@ -11,20 +12,28 @@ export default function Post() {
             <link rel="apple-touch-icon-precomposed" href="/cropped-BCCF-basket-logo-white-5001-180x180.png" />
         </Head>
 
-        <main className={styles.main}>
+        <main className={`${styles.main} main-container`}>
             <div className='post'>
                 <h2 className='no-space-below'>About Dr. Karen Kostroff & her relationship with BCCF</h2>
                 <p className='no-space-above'>by <span>Zahel Nasari</span> | Jan 30, 2021</p>
                 <p>Dr. Karen Michele Kostroff (MD) is a Surgical Oncology Specialist in New Hyde Park, NY, and has over 42 years of experience in the medical field. She is one of New York’s most renowned breast cancer surgeons and is presently Chief of Breast Surgery for the North Shore-Long Island Jewish Health System and operates at both Long Island Jewish Medical Center and North Shore University Hospital. Educated in the six-year medical honors program at Boston University, she then completed her surgical training at New York Hospital-Cornell Medical Center, which included a concentration on cancer surgery and breast disorders at Memorial Sloan-Kettering Cancer Institute. This was followed by additional training in cancer surgery at Brigham and Women’s Hospital of Harvard Medical School.</p>
                 <p>Dr. Kostroff is one of the nation’s leading breast cancer specialists in the diagnosis and treatment of women at risk for genetic-based breast cancer. She has provided consultations for hundreds of women at risk for hereditary breast cancer. The Breast Cancer Comfort Foundation is lucky to have Dr. Kostroff as a part of our team. With her support, we can continue our mission to deliver Comfort Baskets and Chemo Comfort Bags to those who have recently undergone breast cancer surgery and remind them that they are not alone or unloved.</p>
             </div>
+
+            <FB />
         </main>
 
         <style jsx>
           {`
+          .main-container {
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: center;
+          }
+
           .post {
             width: 80%;
-            margin: 3rem auto;
+            margin: 3rem 0 auto;
             color: white;
           }
 
@@ -39,6 +48,18 @@ export default function Post() {
 
           span {
             color: #b83574;
+          }
+
+          @media only screen and (min-width: 1024px) {
+            .main-container {
+              justify-content: space-between;
+              width: 90%;
+              margin: 0 auto;
+            }
+
+            .post {
+              width: calc(100% - 380px);
+            }
           }
           `}
         </style>
