baec382f5be7af50d7bfa66574777d0a2c7cc133
vishalicious213 <vishalicious213@gmail.com>
2022-02-14 10:46:19 -0800

    components/contact.js: adding styles

--- components/contact.js
+++ components/contact.js
@@ -1,36 +1,44 @@
 export default function ContactForm() {
     return (
         <div>
-                <form id='contact-form' name='contact-form' method='POST' data-netlify='true'>
-                    <input type='hidden' name='form-name' value='contact-form' />
-                    <p>Leave a message!</p>
-                    <input
-                        id='name'
-                        name='name'
-                        type='text'
-                        placeholder='name'
-                    />
-                    <input
-                        id='email'
-                        name='email'
-                        type='email'
-                        placeholder='email'
-                    />
-                    <input
-                        id='phone'
-                        name='phone'
-                        type='text'
-                        placeholder='phone'
-                    />
-                    <textarea
-                        id='message'
-                        name='message'
-                        rows='4'
-                        cols='50'
-                        placeholder='message'
-                    />
-                    <button type='submit'>Submit</button>
-                </form>
+            <form id='contact-form' name='contact-form' method='POST' data-netlify='true'>
+                <input type='hidden' name='form-name' value='contact-form' />
+                <p>Leave a message!</p>
+                <input
+                    id='name'
+                    name='name'
+                    type='text'
+                    placeholder='name'
+                />
+                <input
+                    id='email'
+                    name='email'
+                    type='email'
+                    placeholder='email'
+                />
+                <input
+                    id='phone'
+                    name='phone'
+                    type='text'
+                    placeholder='phone'
+                />
+                <textarea
+                    id='message'
+                    name='message'
+                    rows='4'
+                    cols='50'
+                    placeholder='message'
+                />
+                <button type='submit'>Submit</button>
+            </form>
+
+            <style jsx>
+                {`
+                form {
+                    color: white;
+                }
+                `}
+            </style>
         </div>
         
     )
