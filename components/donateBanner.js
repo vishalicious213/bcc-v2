7af564904aa3a2f2f393b9c9db40b7bdf51430c2
vishalicious213 <vishalicious213@gmail.com>
2022-03-24 01:24:18 -0700

    updated components/donateBanner.js

--- components/donateBanner.js
+++ components/donateBanner.js
@@ -3,7 +3,15 @@ import styles from '../styles/Home.module.css'
 export default function donateBanner() {
     return (
         <>
-        
+        <div className='button-container'>
+            <a className='fb icon' target='_blank' rel='noopener' href="https://www.nfggive.com/donation/47-2782120">
+                <button className='button button-1'>NETWORK FOR GOOD</button>
+            </a>
+
+            <a className='fb icon' target='_blank' rel='noopener' href="https://venmo.com/u/breastcancercomfort">
+                <button className='button'>VENMO</button>
+            </a>
+        </div>
         </>
     )
 }
