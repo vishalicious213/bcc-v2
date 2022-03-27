6604c7606bdc3f4a006420ca1272a83a10b3bb88
vishalicious213 <vishalicious213@gmail.com>
2022-03-27 12:27:53 -0700

    drafted Shop component, imported into Boobtique page

---
+++ components/shop.js
@@ -0,0 +1,22 @@
+const Shop = () => {
+    return (
+        <div id="shop">
+        <h2>SHOP</h2>
+
+        <style jsx>
+            {`
+            #shop {
+                display: flex;
+                justify-content: center;
+            }
+
+            h2 {
+                color: white;
+            }
+            `}
+        </style>
+        </div>
+    )
+}
+
+export default Shop
