0deddd308058911859886733ec98f0fdf5e6aebe
vishalicious213 <vishalicious213@gmail.com>
2022-03-29 19:03:03 -0700

    forced commit

--- pages/boobtique.js
+++ pages/boobtique.js
@@ -55,7 +55,7 @@ export default function Boobtique() {
             </div>
           </section>
 
-          <Shop />
+          {/* <Shop /> */}
           
           {/* <Shopify /> */}
           
