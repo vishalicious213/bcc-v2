5d96d6e131777426fefe392edd995cd380feba5d
vishalicious213 <vishalicious213@gmail.com>
2022-04-06 22:09:24 -0700

    updated text in events/2022-05-12

--- pages/events/2022-05-12.js
+++ pages/events/2022-05-12.js
@@ -18,9 +18,10 @@ export default function Post() {
             <div className='post'>
                 <h1 className='no-space-below'>Fundraiser at Scrubz</h1>
                 <p className='no-bot'>Thursday | <span>May 12, 2022</span> (7 - 9 pm)</p>
-                <p className='no-top no-bot'>RSVP required by <span>April 30, 2022</span></p>
+                <p className='no-top no-bot'>RSVP required by <span>April 30, 2022 </span>| <span>$45 </span>per person</p>
                 <p className='no-top no-bot'>Contact: Alicia @ <span>onthego44@yahoo.com</span></p>
-                <p className='no-top'>Call or text <span>(516) 695-2076 </span>for more information!</p>
+                <p className='no-top no-bot'>Call or text <span>(516) 695-2076 </span>for more information!</p>
+                <p className='no-top'>Pay via <a href='https://account.venmo.com/u/breastcancercomfort'><span id='venmo'>Venmo</span></a> or make check payable to <span>Breast Cancer Comfort Foundation</span></p>
                 <div className='poster'>
                   <img src='/events/2022-05-12-scrubz.jpg' alt='Breast Cancer Comfort fundraiser with Scrubz in Farmingdale NY' />
                 </div>
@@ -57,6 +58,16 @@ export default function Post() {
 
           span {
             color: #b83574;
+          }
+
+          #venmo {
+            text-decoration: underline;
+            text-decoration-color: #b83574;
+          }
+
+          #venmo:hover {
+            color: #ff99cc;
+            text-decoration-color: #ff99cc;
           }
 
           .no-top {
