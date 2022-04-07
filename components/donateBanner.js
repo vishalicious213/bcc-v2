ab708068f360a23f43f7e67dc4b02bdb6688009a
vishalicious213 <vishalicious213@gmail.com>
2022-04-06 19:59:54 -0700

    donateBanner: switched order of venmo and network for good buttons

--- components/donateBanner.js
+++ components/donateBanner.js
@@ -7,11 +7,11 @@ export default function DonateBanner() {
             <h2>DONATE:</h2>
 
             <div id='buttons'>
-                <a className='fb icon' target='_blank' rel='noopener' href="https://www.nfggive.com/donation/47-2782120">
-                    <button className='button button-1'>NETWORK FOR GOOD</button>
-                </a>
                 <a className='fb icon' target='_blank' rel='noopener' href="https://venmo.com/u/breastcancercomfort">
-                    <button className='button'>VENMO</button>
+                    <button className='button button-1'>VENMO</button>
+                </a>
+                <a className='fb icon' target='_blank' rel='noopener' href="https://www.nfggive.com/donation/47-2782120">
+                    <button className='button'>NETWORK FOR GOOD</button>
                 </a>
             </div>
         </div>
