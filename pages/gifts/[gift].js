109a94001705806aae7a4637db8550b89f982a14
vishalicious213 <vishalicious213@gmail.com>
2022-04-08 14:09:40 -0700

    [gift].js renders <p>

--- pages/gifts/[gift].js
+++ pages/gifts/[gift].js
@@ -1,37 +1,47 @@
-import { products } from "../../components/shop"
+import { gifts } from "../../components/shop"
 
 const Gift = (props) => {
     console.log(props)
     return (
-        <p>Product</p>
+        <>
+        <p>Gift</p>
+
+        <style jsx>
+            {`
+            p {
+                color: white;
+            }
+            `}
+        </style>
+        </>
     )
 }
 
-export const getStaticPaths = () => {
-    const filenames = products.slug
-    console.log(filenames)
+// export const getStaticPaths = () => {
+//     const filenames = gifts.slug
+//     console.log(filenames)
     
-    const paths = filenames.map(filename => {
-        return {
-            params: {
-                gift: `/gifts/${filename}`
-            }
-        }
-    })
+//     const paths = filenames.map(filename => {
+//         return {
+//             params: {
+//                 gift: `/gifts/${filename}`
+//             }
+//         }
+//     })
 
-    return {
-        paths,
-        fallback: false,
-    }
-}
+//     return {
+//         paths,
+//         fallback: false,
+//     }
+// }
 
-export const getStaticProps = async (context) => {
-    const giftName = context.params.gift
-    return {
-        props: {
-            products
-        }
-    }
-}
+// export const getStaticProps = async (context) => {
+//     const giftName = context.params.gift
+//     return {
+//         props: {
+//             gift
+//         }
+//     }
+// }
 
 export default Gift
