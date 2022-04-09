6bc13ffc2f35cfe41407bb1afdf63849939169ac
vishalicious213 <vishalicious213@gmail.com>
2022-04-09 14:42:50 -0700

    drafted gifts/chemo-comfort-bag & gifts/comfort-bag-with-blanket

---
+++ pages/gifts/comfort-bag-with-blanket.js
@@ -0,0 +1,72 @@
+import { gifts } from '../../components/shop'
+
+let gift = gifts[2]
+
+export default function ComfortBagWithBlanket() {
+    // console.log(gift)
+    return (
+        <div className='gift'>
+            <div className='gift-txt'>
+                <h1>{gift.name}</h1>
+                <div className='pricing'>
+                    <p><span className='pricing-detail'><b>Price: </b></span>{`$${gift.price/100}.00`}</p>
+                    <p><span className='pricing-detail'><b>Quantity available:</b></span> {gift.quantity}</p>
+                </div>
+            </div>
+
+            <div className='gift-img'>
+                <img className='img' src={gift.img} alt={gift.alt} />
+            </div>
+
+            <p className='gift-txt'>{gift.desc}</p>
+            
+            <style jsx>
+                {`
+                .gift {
+                    color: gainsboro;
+                    width: 100%;
+                    display: flex;
+                    flex-direction: column;
+                    align-items: center;
+                    margin-bottom: 3rem;
+                }
+
+                .gift-txt {
+                    width: 100%;
+                    padding-left: 2rem;
+                    margin-bottom: 0;
+                    margin-top: 0;
+                }
+
+                h1 {
+                    font-size: 2rem;
+                    margin-bottom: 0;
+                    color: #b01e65;
+                }
+
+                .pricing {
+                    display: flex;
+                    justify-content: space-between;
+                    padding-right: 2rem;
+                    width: 100%;
+                }
+                
+                .pricing-detail {
+                    color: gainsboro;
+                    color: #f5d3e4;
+                }
+
+                .gift-img {
+                    width: 50%;
+                    margin-bottom: 2rem;
+                    margin-top: 1rem;
+                }
+                
+                .img {
+                    width: 100%;
+                }
+                `}
+            </style>
+        </div>
+    )
+}
