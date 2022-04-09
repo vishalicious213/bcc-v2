4d073f9da782c93c3e1f22d62d5348cbee9b29ef
vishalicious213 <vishalicious213@gmail.com>
2022-04-09 14:33:30 -0700

    styling gifts/comfort-basket

--- pages/gifts/comfort-basket.js
+++ pages/gifts/comfort-basket.js
@@ -6,16 +6,62 @@ export default function ComfortBasket() {
     // console.log(gift)
     return (
         <div className='gift'>
-            <h1>{gift.name}</h1>
-            <p>{`Price: $${gift.price / 100}.00`}</p>
-            <p>Quantity available: {gift.quantity}</p>
-            <img src={gift.img} alt={gift.alt} />
-            <p>{gift.desc}</p>
+            <div className='gift-txt'>
+                <h1>{gift.name}</h1>
+                <div className='pricing'>
+                    <p><span className='pricing-detail'><b>Price: </b></span>{`$${gift.price/100}.00`}</p>
+                    <p><span className='pricing-detail'><b>Quantity available:</b></span> {gift.quantity}</p>
+                </div>
+            </div>
+
+            <div className='gift-img'>
+                <img className='img' src={gift.img} alt={gift.alt} />
+            </div>
+
+            <p className='gift-txt'>{gift.desc}</p>
             
             <style jsx>
                 {`
                 .gift {
                     color: gainsboro;
+                    width: 100%;
+                    display: flex;
+                    flex-direction: column;
+                    align-items: center;
+                    margin-bottom: 3rem;
+                }
+
+                .gift-txt {
+                    width: 100%;
+                    padding-left: 2rem;
+                    margin-bottom: 0;
+                    margin-top: 0;
+                }
+
+                h1 {
+                    font-size: 2rem;
+                    margin-bottom: 0;
+                    color: #b01e65;
+                }
+
+                .pricing {
+                    display: flex;
+                    justify-content: space-between;
+                    padding-right: 2rem;
+                    width: 50%;
+                }
+                
+                .pricing-detail {
+                    color: gainsboro;
+                    color: #f5d3e4;
+                }
+
+                .gift-img {
+                    width: 50%;
+                }
+                
+                .img {
+                    width: 100%;
                 }
                 `}
             </style>
@@ -23,11 +69,34 @@ export default function ComfortBasket() {
     )
 }
 
-// id: 0,
-// name: 'Comfort basket',
-// price: 2500,
-// desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sed enim at mi aliquam egestas. Maecenas accumsan commodo turpis, consequat efficitur metus interdum eget. Donec fermentum massa massa. Donec viverra nisl id consectetur vestibulum. Maecenas ac dapibus justo, nec commodo quam. Praesent commodo mi ut rutrum bibendum. Donec eu ultricies urna. Cras scelerisque massa leo, sed pharetra ligula egestas quis. Etiam enim nisl, pulvinar vitae lacus sed, fringilla maximus arcu. Integer vitae lectus porttitor, facilisis leo eget, aliquet leo. Suspendisse malesuada ut mauris eu scelerisque. Ut vitae mauris id tellus hendrerit molestie vel vitae libero.',
-// img: '/breast-cancer-ribbon.png',
-// alt: '',
-// quantity: 10,
-// slug: 'comfort-basket',
+{/* <div key={gift.id} className='gift'>
+<div className='gift-img'>
+    <img className='img' src={gift.img} alt={gift.alt} />
+</div>
+<div className='gift-txt'>
+    <Link href={`/gifts/${gift.slug}`}>
+        <a><h3>{gift.name}</h3></a>
+    </Link>
+    <div className='pricing'>
+        <p><span className='pricing-detail'><b>Price: </b></span>{`$${gift.price/100}.00`}</p>
+        <p><span className='pricing-detail'><b>Quantity available:</b></span> {gift.quantity}</p>
+    </div>
+    <p className='desc'>{gift.desc}</p>
+</div>
+</div>
+
+
+@media only screen and (min-width: 1024px) {
+    .gift {
+        flex-direction: row;
+        align-items: center;
+    }
+
+    .gift-img {
+        width: 20%;
+    }
+
+    .gift-txt {
+        width: 80%;
+    }
+} */}
