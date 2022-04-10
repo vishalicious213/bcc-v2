0b74327c988c5993430339feaa0aa9e4399cfd87
vishalicious213 <vishalicious213@gmail.com>
2022-04-10 14:45:06 -0700

    updated Boobtique text to reflect active shop

--- pages/boobtique.js
+++ pages/boobtique.js
@@ -25,14 +25,14 @@ export default function Boobtique() {
             </div>
             <div className='gift-text-container'>
                 <h2 className='h2'>Welcome to our Shop!</h2>
-                <p className='no-space-above'>We are getting ready to open our doors to the public. If you know someone who is undergoing treatment for breast cancer and you don’t know what to give, then let us help you.</p>
+                <p className='no-space-above'>Our doors are now open to the public! If you know someone who is undergoing treatment for breast cancer and you don’t know what to give, then let us help you.</p>
                 <p>Handmade hearts, comfort bags, and custom gift baskets will be available soon. Select any one of our specialty items and we will ship to you, or anyone you choose, within 48 hours.</p>
             </div>
           </section>
 
-          <div className='button-container'>
+          {/* <div className='button-container'>
             <button className='button'>VISIT OUR STORE</button>
-          </div>
+          </div> */}
 
           <section className='grey-container gift-container'>
             <div className='gift-img-container-top'>
