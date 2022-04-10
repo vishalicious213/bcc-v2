200075923ebf4dd430af4dfdecb697b21fc34804
vishalicious213 <vishalicious213@gmail.com>
2022-04-10 15:34:58 -0700

    useCart: added addItemToCart & removeItemFromCart

--- hooks/useCart.js
+++ hooks/useCart.js
@@ -2,8 +2,24 @@ import { useState } from 'react'
 
 const useCart = () => {
     const [cart, setCart] = useState([])
+
+    // takes item ID and quantity. quantity defaults to 1 if none passed in.
+    const addItemToCart = (id, qty = 1) => {
+        setCart([...cart, { id, qty }])
+    }
+
+    // returns a new cart with items that match the ID removed
+    const removeItemFromCart = (id) => {
+        const newCart = cart.filter(item => {
+            return item.id != id
+        })
+        setCart(newCart)
+    }
+
     return {
         cart,
+        addItemToCart,
+        removeItemFromCart
     }
 }
 
