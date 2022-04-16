7b84fd1e09550065216b4bb7846c446c06e7cda4
vishalicious213 <vishalicious213@gmail.com>
2022-04-16 15:45:21 -0700

    drafted /success page, for successful Stripe payment

---
+++ pages/success.js
@@ -0,0 +1,16 @@
+const Success = () => {
+    return (
+        <div className='success'>
+            <h2>Payment successful</h2>
+            <p>Thank you for your purchase!</p>
+
+            <style jsx>
+                {`
+                .success {
+                    color: white;
+                }
+                `}
+            </style>
+        </div>
+    )
+}
