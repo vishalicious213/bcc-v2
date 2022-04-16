d5b18dc00c9d7cec9784474ae67db304bcc5580a
vishalicious213 <vishalicious213@gmail.com>
2022-04-16 15:19:38 -0700

    /checkout: styling

--- pages/checkout.js
+++ pages/checkout.js
@@ -12,7 +12,7 @@ const Checkout = () => {
             <h1>Checkout</h1>
 
             {cart.length > 0 ? (
-                <div className='cart-body'>
+                <div className='checkout-body'>
                     {cart.map((item) => {
                         return (
                             <div className='item' key={item.id}>
@@ -25,7 +25,11 @@ const Checkout = () => {
                             </div>
                         )
                     })}
-                    <p className='total'>{`Total: $${total / 100}.00`}</p>
+                    <div className='total'>
+                        <p className='test'></p>
+                        <p className='test'></p>
+                        <p className='test'>{`Total: $${total / 100}.00`}</p>
+                    </div>
                     <button onClick={processPayment}>Process Payment</button>
                 </div>
             ) : (
@@ -35,7 +39,37 @@ const Checkout = () => {
             <style jsx>
                 {`
                 .checkout {
-                    color: white;
+                    color: gainsboro;
+                }
+
+                h1 {
+                    margin: 1rem 1rem 0 1rem;
+                    padding-bottom: .5rem;
+                    border-bottom: 2px solid #b01e65;
+                }
+
+                .checkout-body {
+                    padding: 1rem;
+                }
+
+                h2 {
+                    margin-bottom: 0;
+                }
+
+                .pricing {
+                    display: grid;
+                    grid-gap: 2rem;
+                    grid-template-columns: 1fr 1fr 1fr;
+                    margin-bottom: 1rem;
+                    border-bottom: 1px solid #b01e65;
+                }
+
+                .total {
+                    display: grid;
+                    grid-gap: 2rem;
+                    grid-template-columns: 1fr 1fr 1fr;
+                    font-size: 1.25rem;
+                    font-weight: 600;
                 }
                 `}
             </style>
