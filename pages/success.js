fdd2ed6a01e40b0a0d42eba60effadc9578def7c
vishalicious213 <vishalicious213@gmail.com>
2022-04-16 16:00:34 -0700

    /success: basic styling

--- pages/success.js
+++ pages/success.js
@@ -11,13 +11,28 @@ const Success = () => {
 
     return (
         <div className='success'>
-            <h2>Payment successful</h2>
-            <p>Thank you for your purchase!</p>
+            <h2>Payment successful!</h2>
+            <p>Thank you for your donation!</p>
+            <p>Your gifts are on their way.</p>
 
             <style jsx>
                 {`
                 .success {
+                    display: flex;
+                    flex-direction: column;
+                    align-items: center;
+                    color: gainsboro;
+                    margin-top: 4rem;
+                    margin-bottom: 4rem;
+                }
+
+                h2 {
                     color: white;
+                    margin-bottom: 0;
+                }
+
+                p {
+                    margin-bottom: 0;
                 }
                 `}
             </style>
