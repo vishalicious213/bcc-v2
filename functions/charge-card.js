2cea7f3e030c4285fd3745cad5d6caa0b2dba88f
vishalicious213 <vishalicious213@gmail.com>
2022-04-17 13:47:02 -0700

    charge-card: took qty and id from checkout and combined it with item details from gifts array into new array of data. pricing is read from backend, so its not tampered with by potential frontend hackers. will be able to send that to stripe safely.

--- functions/charge-card.js
+++ functions/charge-card.js
@@ -1,9 +1,24 @@
 const gifts = require('../content/gifts')
+const giftsArray = gifts.gifts
+// gifts was returning an object with the gifts array inside of it
+// giftsArray let us access those as an array
 
 exports.handler = async (event, context) => {
     // console.log(event.body)
-    console.log(gifts)
-    const { cart } = event.body
+    // console.log('giftsArray', giftsArray)
+    const { cart } = JSON.parse(event.body)
+    // console.log('node cart', cart)
+
+    const cartWithGifts = cart.map(({ id, qty }) => {
+        const gift = giftsArray.find((g) => g.id === id)
+        return {
+            ...gift,
+            qty,
+        }
+    })
+
+    console.log(cartWithGifts)
+    
     // talking to Stripe
     // charging the card 
     return {
