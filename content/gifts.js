cd5bd1a2ec60111303fd357f73516fa814b57661
vishalicious213 <vishalicious213@gmail.com>
2022-04-16 20:33:54 -0700

    updated gift pricing as per Debi

--- content/gifts.js
+++ content/gifts.js
@@ -2,7 +2,7 @@ export const gifts = [
     {
         id: 0,
         name: 'Comfort basket',
-        price: 2500,
+        price: 2200,
         desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sed enim at mi aliquam egestas. Maecenas accumsan commodo turpis, consequat efficitur metus interdum eget. Donec fermentum massa massa. Donec viverra nisl id consectetur vestibulum. Maecenas ac dapibus justo, nec commodo quam. Praesent commodo mi ut rutrum bibendum. Donec eu ultricies urna. Cras scelerisque massa leo, sed pharetra ligula egestas quis. Etiam enim nisl, pulvinar vitae lacus sed, fringilla maximus arcu. Integer vitae lectus porttitor, facilisis leo eget, aliquet leo. Suspendisse malesuada ut mauris eu scelerisque. Ut vitae mauris id tellus hendrerit molestie vel vitae libero.',
         img: '/breast-cancer-ribbon.png',
         alt: '',
@@ -12,7 +12,7 @@ export const gifts = [
     {
         id: 1,
         name: 'Chemo comfort bag (pattern varies)',
-        price: 2500,
+        price: 2000,
         desc: 'Integer et sapien lobortis, eleifend quam ut, pellentesque quam. Interdum et malesuada fames ac ante ipsum primis in faucibus. Proin ullamcorper odio ac diam fringilla, ac vulputate elit dignissim. Pellentesque non metus nisl. Nam placerat libero vitae rhoncus facilisis. Praesent posuere, arcu rhoncus tristique ullamcorper, magna diam tincidunt nisi, sit amet porta erat tortor sit amet nisi. Suspendisse quis blandit ligula, nec molestie purus.',
         img: '/breast-cancer-ribbon.png',
         alt: '',
