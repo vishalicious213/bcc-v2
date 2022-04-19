c5bf7a1b044ed4b421b65c73084ce68273e35b3f
vishalicious213 <vishalicious213@gmail.com>
2022-04-18 18:44:20 -0700

    comfort-basket: removed pics array. will house pics arrays in carousel component and send prop to tell it which set to use

--- pages/gifts/comfort-basket.js
+++ pages/gifts/comfort-basket.js
@@ -3,12 +3,6 @@ import useCart from '../../hooks/useCart'
 import GiftsCarousel from '../../components/gifts-carousel'
 
 const gift = gifts[0]
-const pics = [
-    {id: 0, img: '/gifts/comfort-basket-01.jpg', alt: 'comfort basket with animal'},
-    {id: 1, img: '/gifts/comfort-basket-02.jpg', alt: 'comfort basket with dark tulle'},
-    {id: 2, img: '/gifts/comfort-basket-03.jpg', alt: 'comfort basket with reusable shopping bag'},
-    // {id: 0, img: '/gifts/comfort-basket-01.jpg', alt: ''}
-]
 
 export default function ComfortBasket() {
     const { cart, addItemToCart, removeItemFromCart } = useCart()
