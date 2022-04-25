4568ce44968886cd76564aaf706bf180349cabad
vishalicious213 <vishalicious213@gmail.com>
2022-04-25 14:35:14 -0700

    serverless-test: got parcelId, fromAddressId & toAddressId

--- functions/serverless-test.js
+++ functions/serverless-test.js
@@ -2,55 +2,84 @@ require('dotenv').config()
 const Easypost = require('@easypost/api')
 const api = new Easypost(process.env.EASYPOST_TEST_API_KEY)
 
-let parcelId
-let fromAddressId
-let toAddressId
+// let parcelId
+// let fromAddressId
+// let toAddressId
 
-const fromAddress = new api.Address({
-    company: 'EasyPost',
-    street1: '417 Montgomery Street',
-    street2: '5th Floor',
-    city: 'San Francisco',
-    state: 'CA',
-    zip: '94104',
-    phone: '415-528-7555'
-});
-// fromAddress.save().then(console.log);
-fromAddress.save().then((res) => {
-    fromAddressId = res.id
-    console.log('fromAddressId', fromAddressId)
-});
+async function getFromAddress() {
+    const fromAddress = new api.Address({
+        company: 'EasyPost',
+        street1: '417 Montgomery Street',
+        street2: '5th Floor',
+        city: 'San Francisco',
+        state: 'CA',
+        zip: '94104',
+        phone: '415-528-7555'
+    });
+    // fromAddress.save().then(console.log);
+    let id = await fromAddress.save().then((res) => {
+        // fromAddressId = res.id
+        // console.log('fromAddressId', fromAddressId)
+        return res.id
+    });
 
-const toAddress = new api.Address({
-    name: 'George Costanza',
-    company: 'Vandelay Industries',
-    street1: '1 E 161st St.',
-    city: 'Bronx',
-    state: 'NY',
-    zip: '10451'
-});
-// toAddress.save().then(console.log);
-toAddress.save().then((res) => {
-    toAddressId = res.id
-    console.log('toAddressId', toAddressId)
-});
+    return id
+}
 
-const parcel = new api.Parcel({
-    length: 9,
-    width: 6,
-    height: 2,
-    weight: 10,
-});
-// parcel.save().then(console.log);
-parcel.save().then((res) => {
-    parcelId = res.id
-    console.log('parcelId', parcelId)
-});
+
+async function getToAddress() {
+    const toAddress = new api.Address({
+        name: 'George Costanza',
+        company: 'Vandelay Industries',
+        street1: '1 E 161st St.',
+        city: 'Bronx',
+        state: 'NY',
+        zip: '10451'
+    });
+    // toAddress.save().then(console.log);
+    let id = await toAddress.save().then((res) => {
+        // let toAddressId = res.id
+        // console.log('toAddressId', toAddressId)
+        // return toAddressId
+        return res.id
+    });
+
+    console.log('id', id)
+
+    return id
+}
+
+
+async function getParcel() {
+    const parcel = new api.Parcel({
+        length: 9,
+        width: 6,
+        height: 2,
+        weight: 10,
+    });
+    // parcel.save().then(console.log);
+    let id = await parcel.save().then((res) => {
+        // let parcelId = res.id
+        // console.log('parcelId', parcelId)
+        // return parcelId
+        return res.id
+    });
+
+    // console.log('parcelId outside', parcelId)
+    return id
+}
 
 exports.handler =  async function(event, context) {
     // console.log("EVENT: \n" + JSON.stringify(event, null, 2))
     // return context.logStreamName
+    let toAddressId = await getToAddress()
+    let fromAddressId = await getFromAddress()
+    let parcelId = await getParcel()
     console.log('serverless-test response')
+    // console.log('parcelId handler', getParcel())
+    // console.log('toAddressId handler', getToAddress())
+    // console.log(parcelId, fromAddressId, toAddressId)
+    // console.log(parcel)
     // console.log(api)
 
     return {
@@ -58,9 +87,9 @@ exports.handler =  async function(event, context) {
         body: JSON.stringify({
             // test: 'SERVERLESS TEST SUCCESSFUL',
             // easypost: api,
-            to_address: toAddress,
-            from_address: fromAddress,
-            parcel: parcel,
+            to_address: toAddressId || 'no data',
+            from_address: fromAddressId || 'no data',
+            parcel: parcelId,
         }),
     }    
 }
