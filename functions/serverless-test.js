80862cf95a6c21839c5390b7aa45e907f05821ac
vishalicious213 <vishalicious213@gmail.com>
2022-04-25 11:14:20 -0700

    serverless-test: added fromAddress

--- functions/serverless-test.js
+++ functions/serverless-test.js
@@ -2,6 +2,17 @@ require('dotenv').config()
 const Easypost = require('@easypost/api')
 const api = new Easypost(process.env.EASYPOST_TEST_API_KEY)
 
+const fromAddress = new api.Address({
+    company: 'EasyPost',
+    street1: '417 Montgomery Street',
+    street2: '5th Floor',
+    city: 'San Francisco',
+    state: 'CA',
+    zip: '94104',
+    phone: '415-528-7555'
+});
+fromAddress.save().then(console.log);
+
 exports.handler =  async function(event, context) {
     // console.log("EVENT: \n" + JSON.stringify(event, null, 2))
     // return context.logStreamName
