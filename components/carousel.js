9c2419fe6385b55e42315287c94eb9091db594d8
vishalicious213 <vishalicious213@gmail.com>
2022-06-02 18:31:21 -0700

    carousel: changed image order, moved archived image files into /removed and added 5 images for edit

--- components/carousel.js
+++ components/carousel.js
@@ -59,67 +59,67 @@ export default function Carousel() {
             </Head>
 
             <Slider {...settings}>
-                {/* <div>
-                    <img src='/carousel-images/carousel-01.jpg' alt='Breast Cancer Comfort event' />
-                </div> */}
                 <div>
                     <img src='/carousel-images/carousel-02.jpg' alt='Breast Cancer Comfort comfort basket' />
                 </div>
                 <div>
                     <img src='/carousel-images/carousel-03.jpg' alt='Breast Cancer Comfort, high school students making baskets' />
                 </div>
-                {/* <div>
-                    <img src='/carousel-images/carousel-04.jpg' alt='Breast Cancer Comfort comfort baskets and chemo bags' />
-                </div> */}
+                <div>
+                    <img src='/carousel-images/carousel-09.jpg' alt='Breast Cancer Comfort comfort baskets' />
+                </div>
                 <div>
                     <img src='/carousel-images/carousel-05.jpg' alt='Breast Cancer Comfort event' />
                 </div>
                 <div>
                     <img src='/carousel-images/carousel-06.jpg' alt='Breast Cancer Comfort event' />
                 </div>
+                <div>
+                    <img src='/carousel-images/carousel-13.jpg' alt='Breast Cancer Comfort comfort basket' />
+                </div>
+                <div>
+                    <img src='/carousel-images/carousel-10.jpg' alt='Breast Cancer Comfort, Debi Cavolo, Salt & Pepper, NY Mets' />
+                </div>
+                <div>
+                    <img src='/carousel-images/carousel-17.jpg' alt='Breast Cancer Comfort comfort basket' />
+                </div>
+                <div>
+                    <img src='/carousel-images/carousel-12.jpg' alt='Breast Cancer Comfort, Debi Cavolo' />
+                </div>
+                <div>
+                    <img src='/carousel-images/carousel-19.jpg' alt='Breast Cancer Comfort comfort basket with letter from Renae Eager' />
+                </div>
+                <div>
+                    <img src='/carousel-images/carousel-16.jpg' alt='Breast Cancer Comfort, high school girls making maskets' />
+                </div>
+                <div>
+                    <img src='/carousel-images/carousel-18.jpg' alt='Breast Cancer Comfort, Raven Thomas and her mother' />
+                </div>
+                <div className='text-container'>
+                    <p className='image-text'>The Breast Cancer Comfort Foundation recently paid our practice a visit to drop off some chemotherapy comfort bags. These curated collections of personal care items are designed to help and support patients who have recently undergone surgery for breast cancer.</p>
+                    <img src='/carousel-images/carousel-20.jpg' alt='Breast Cancer Comfort comfort basket' />
+                </div>
+                {/* <div>
+                    <img src='/carousel-images/carousel-01.jpg' alt='Breast Cancer Comfort event' />
+                </div> */}
+                {/* <div>
+                    <img src='/carousel-images/carousel-04.jpg' alt='Breast Cancer Comfort comfort baskets and chemo bags' />
+                </div> */}
                 {/* <div>
                     <img src='/carousel-images/carousel-07.jpg' alt='Breast Cancer Comfort basket items' />
                 </div> */}
                 {/* <div>
                     <img src='/carousel-images/carousel-08.jpg' alt='Breast Cancer Comfort, Sana' />
                 </div> */}
-                <div>
-                    <img src='/carousel-images/carousel-09.jpg' alt='Breast Cancer Comfort comfort baskets' />
-                </div>
-                <div>
-                    <img src='/carousel-images/carousel-10.jpg' alt='Breast Cancer Comfort, Debi Cavolo, Salt & Pepper, NY Mets' />
-                </div>
                 {/* <div>
                     <img src='/carousel-images/carousel-11.jpg' alt='Breast Cancer Comfort comfort basket collage' />
                 </div> */}
-                <div>
-                    <img src='/carousel-images/carousel-12.jpg' alt='Breast Cancer Comfort, Debi Cavolo' />
-                </div>
-                <div>
-                    <img src='/carousel-images/carousel-13.jpg' alt='Breast Cancer Comfort comfort basket' />
-                </div>
                 {/* <div>
                     <img src='/carousel-images/carousel-14.jpg' alt='Breast Cancer Comfort, Sana' />
                 </div> */}
                 {/* <div>
                     <img src='/carousel-images/carousel-15.jpg' alt='Breast Cancer Comfort comfort basket collage' />
                 </div> */}
-                <div>
-                    <img src='/carousel-images/carousel-16.jpg' alt='Breast Cancer Comfort, high school girls making maskets' />
-                </div>
-                <div>
-                    <img src='/carousel-images/carousel-17.jpg' alt='Breast Cancer Comfort comfort basket' />
-                </div>
-                <div>
-                    <img src='/carousel-images/carousel-18.jpg' alt='Breast Cancer Comfort, Raven Thomas and her mother' />
-                </div>
-                <div>
-                    <img src='/carousel-images/carousel-19.jpg' alt='Breast Cancer Comfort comfort basket with letter from Renae Eager' />
-                </div>
-                <div className='text-container'>
-                    <p className='image-text'>The Breast Cancer Comfort Foundation recently paid our practice a visit to drop off some chemotherapy comfort bags. These curated collections of personal care items are designed to help and support patients who have recently undergone surgery for breast cancer.</p>
-                    <img src='/carousel-images/carousel-20.jpg' alt='Breast Cancer Comfort comfort basket' />
-                </div>
             </Slider>
 
             <style jsx>
