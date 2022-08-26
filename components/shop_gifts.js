be8c8848c7be7ef994e6535d419a32d67f5bf246
vishalicious213 <vishalicious213@gmail.com>
2022-08-25 18:05:45 -0700

    shop_gifts: mapping through items and rendering data

--- components/shop_gifts.js
+++ components/shop_gifts.js
@@ -4,9 +4,17 @@ export default function ShopGifts(gifts) {
 
     return (
         <div id="gifts-main">
-            <div>
-                GIFT ITEMS
-            </div>
+            <div>GIFT ITEMS</div>
+            <section className='gifts'>
+                    {items.map(item =>
+                        <div key={item.id} className='gift'>
+                            <h2>{item.name}</h2>
+                            <p>Donation: ${item.price/100}.00</p>
+                            <p>{item.short}</p>
+                            <p>{item.desc}</p>
+                        </div>
+                    )}
+                </section>            
 
             <style jsx>
                 {`
