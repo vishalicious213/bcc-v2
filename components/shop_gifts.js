c5807ebe3a23b9f90748f39c21b8a2972a6e2657
vishalicious213 <vishalicious213@gmail.com>
2022-08-25 18:35:17 -0700

    shop_gifts: styling gifts

--- components/shop_gifts.js
+++ components/shop_gifts.js
@@ -9,10 +9,12 @@ export default function ShopGifts(gifts) {
             <section className='gifts'>
                     {items.map(item =>
                         <div key={item.id} className='gift' style={{backgroundImage: `url(${item.img})`}}>
-                            <h3>{item.name}</h3>
-                            <p className='pricing-detail'><b>Donation:</b> <span className='price'>${item.price/100}.00</span></p>
-                            <p>{item.short}</p>
-                            <p>{item.desc}</p>
+                            <div className="gift-txt">
+                                <h3>{item.name}</h3>
+                                <p className='pricing-detail'><b>Donation:</b> <span className='price'>${item.price/100}.00</span></p>
+                                <p>{item.short}</p>
+                                {/* <p>{item.desc}</p> */}
+                            </div>
                         </div>
                     )}
                 </section>            
@@ -39,13 +41,25 @@ export default function ShopGifts(gifts) {
                     width: 100%;
                     display: flex;
                     flex-direction: column;
-                    justify-content: space-between;
+                    justify-content: flex-end;
                     background-position: center;
                     background-repeat: no-repeat;
                     background-size: cover;
                     border-bottom: 1px solid #b01e65;
                     min-height: 75vh;
                     margin-bottom: 2rem;
+                }
+
+                .gift-txt {
+                    width: 100%;
+                    min-height: 50%;
+                    padding-left: 2rem;
+                    padding-right: 2rem;
+                    padding-bottom: 2rem;
+                    background: rgba(0, 0, 0, 0.5);
+                    display: flex;
+                    flex-direction: column;
+                    justify-content: space-between;
                 }
 
                 h3 {
