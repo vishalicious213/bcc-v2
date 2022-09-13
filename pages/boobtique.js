4885869644186276c62423a677b404d5e7fe4f01
vishalicious213 <vishalicious213@gmail.com>
2022-09-12 18:27:38 -0700

    drafted components/order_form (based on components/contact)

--- pages/boobtique.js
+++ pages/boobtique.js
@@ -2,7 +2,8 @@ import Head from 'next/head'
 // import Shop from '../components/shop'
 import useCart from '../hooks/useCart'
 import { gifts } from '../content/gifts'
-import ShopGifts from '../components/shop_gifts'
+// import ShopGifts from '../components/shop_gifts'
+import ShopGifts from '../components/shop_direct'
 import styles from '../styles/Home.module.css'
 
 export default function Boobtique() {
