538c7b64f0f7b9c7264dcce3bbcfedc0047e13b5
vishalicious213 <vishalicious213@gmail.com>
2022-09-27 20:15:15 -0700

    drafted components/shop_direct_buttons

---
+++ components/shop_direct_buttons.js
@@ -0,0 +1,64 @@
+import { useState } from 'react'
+
+export default function shopDirectButtons() {
+    const [quantity, setQuantity] = useState(0)
+
+    const addOne = () => {
+        setQuantity(quantity + 1)
+    }
+
+    const removeOne = () => {
+        setQuantity(quantity - 1)
+    }
+
+    return (
+        <div className='buttons'>
+            <button className='itemButton' onClick={() => addOne()}>+</button>
+            <span className='itemQuantity'>{quantity}</span>
+            <button className='itemButton' onClick={() => removeOne()}>-</button>   
+
+            <style jsx>
+                {`
+                .buttons {
+                    display: flex;
+                    justify-content: space-around;
+                }
+
+                .itemQuantity {
+                    color: gainsboro;
+                    background-color: rgba(0, 0, 0, 0.2);
+                    font-size: 1.1rem;
+                    width: 5rem;
+                    padding: .5rem 0;
+                    text-align: center;
+                }
+
+                .itemButton {
+                    width: 5rem;
+                }
+
+                .button {
+                    display: flex;
+                    justify-content: center;
+                }
+
+                button {
+                    color: gainsboro;
+                    background-color: #b01e65;
+                    border: none;
+                    font-size: 1.1rem;
+                    height: 2rem;
+                    width: 45%;
+                    padding: .5rem 0;
+                    border-radius: 1rem;
+                }
+
+                button:hover {
+                    color: white;
+                    background-color: deeppink;
+                }                
+                `}
+            </style>                                 
+        </div>        
+    )
+}
