f86100a411cfe6f56f10da14e1a649755baa7d0e
vishalicious213 <vishalicious213@gmail.com>
2022-09-28 17:27:25 -0700

    useShopDirectCart: added functions to add/remove items from shopDirectCart

--- hooks/useShopDirectCart.js
+++ hooks/useShopDirectCart.js
@@ -3,6 +3,16 @@ import { useState } from "react"
 const useShopDirectCart = () => {
     const [shopDirectCart, setShopDirectCart] = useState([])
 
+    const addItemToShopDirectCart = (id, qty = 1) => {
+        setShopDirectCart([...shopDirectCart, { id, qty }])
+    }
+
+    const removeItemFromShopDirectCart = (id) => {
+        const newShopDirectCart = shopDirectCart.filter(item => {
+            return item.id != id
+        })
+    }
+
     return {
         shopDirectCart,
     }
