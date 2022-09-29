95b6fd3f6c299d6e6cf32dc13187af2a6c04a04e
vishalicious213 <vishalicious213@gmail.com>
2022-09-29 13:37:11 -0700

    moved useShopDirectCart (hook) functions into ShopDirectCart (context)

--- context/ShopDirectCart.js
+++ context/ShopDirectCart.js
@@ -1,11 +1,48 @@
-import { createContext } from "react"
+import { createContext, useState, useEffect } from "react"
 import { Context } from "./Cart"
 
 export const ShopDirectCartContext = createContext()
 
 const ShopDirectCart = ({children}) => {
+    const getInitialShopDirectCart = () => JSON.parse(localStorage.getItem('shopDirectCart'))
+    const [shopDirectCart, setShopDirectCart] = useState([])
+
+    useEffect(() => {
+        const initialShopDirectCart = getInitialShopDirectCart()
+        if (initialShopDirectCart) {
+            setShopDirectCart(initialShopDirectCart)
+        }
+    }, [])
+
+    useEffect(() => {
+        // write to localStorage
+        localStorage.setItem('shopDirectCart', JSON.stringify(shopDirectCart))
+    }, [shopDirectCart])
+
+    const addItemToShopDirectCart = (id, qty = 1) => {
+        const item = shopDirectCart.find(i => i.id === id)
+        
+        if (item) {
+            item.qty += qty
+            setShopDirectCart([...shopDirectCart])
+        } else {
+            setShopDirectCart([...shopDirectCart, { id, qty }])
+        }
+
+        console.log('hook', shopDirectCart)
+    }
+
+    const removeItemFromShopDirectCart = (id) => {
+        const newShopDirectCart = shopDirectCart.filter(item => {
+            return item.id != id
+        })
+        setShopDirectCart(newShopDirectCart)
+    }
+
     const exposed = {
-        test: 'vish',
+        shopDirectCart,
+        addItemToShopDirectCart,
+        removeItemFromShopDirectCart,
     }
 
     return <ShopDirectCartContext.Provider value={exposed}>{children}</ShopDirectCartContext.Provider>
